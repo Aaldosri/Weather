a87d433917a265aa2231d73d1c3834c2382d2a5a
GeNeRaL <abood.v000@email.com>
2025-02-13 02:05:36 +0300

    Begining

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import CloudIcon from "@mui/icons-material/Cloud";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header></header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <main className="w-screen">
+        <Container maxWidth="sm" style={{ background: "orange" }}>
+          {/* CONTEANT CONTAINER */}
+
+          <div className="h-screen flex justify-center items-center bg-green-500">
+            {/* CARD */}
+            <div className="bg-[rgba(250,235,215,0.418)] rounded-[10px] p-[10px] shadow-[0px_11px_1px_rgba(0,0,0,0.05)]">
+              {/* CONTENT */}
+              <div>
+                {/* CITY & TIME */}
+                <div className="flex items-end justify-start p-3" dir="rtl">
+                  <Typography className="mr-[20px]" variant="h2">
+                    الرياض
+                  </Typography>
+                  <Typography className="mr-[20px]" variant="h5">
+                    الاحد 13-2-2025
+                  </Typography>
+                </div>
+                <hr />
+
+                {/* CONTAINER OF DEGREE + CLOUD ICON */}
+
+                <div
+                  style={{ display: "flex", justifyContent: "space-around" }}
+                >
+                  {/* DEGREE & DESCRIPTION */}
+                  <div>
+                    {/* TEMP */}
+                    <div>
+                      <Typography className="mr-[20px]" variant="h1" dir="rtl">
+                        11
+                      </Typography>
+
+                      {/* TODO: TEMP IMAGE */}
+                    </div>
+                    {/* === TEMP === */}
+                    <Typography className="mr-[20px]" variant="h2" dir="rtl">
+                      سحابة
+                    </Typography>
+                    {/* MIN & MAX */}
+                    <div>
+                      <Typography className="mr-[20px]" variant="h6" dir="rtl">
+                        الصغرى: 25
+                      </Typography>
+
+                      <Typography className="mr-[20px]" variant="h6" dir="rtl">
+                        الكبرى: 30
+                      </Typography>
+                    </div>
+                  </div>
+                  {/* === DEGREE & DESCRIPTION === */}
+
+                  <CloudIcon
+                    className="text-[200px] text-white"
+                    style={{ fontSize: "200px" }}
+                  />
+                </div>
+                {/* === CONTAINER OF DEGREE + CLOUD ICON === */}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
